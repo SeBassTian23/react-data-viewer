c45148a10d1236eab5615e7c075c17a654936a08
Sebastian <sebastian.kuhlgert@gmail.com>
2025-09-29 00:46:17 +0200

    move anova function to utils

--- src/utils/statistics/oneWayANOVA.jsx
+++ src/utils/statistics/oneWayANOVA.jsx
@@ -8,78 +8,78 @@ import jStat from 'jstat'
  * @returns {Object} ANOVA results
  */
 export default function oneWayANOVA(groups, labels = null) {
-    if (!Array.isArray(groups) || groups.length < 2) {
-        throw new Error('Need at least 2 groups');
-    }
-    
-    const k = groups.length; // number of groups
-    const groupSizes = groups.map(g => g.length);
-    const n = jStat.sum(groupSizes); // total sample size
-    
-    if (labels === null) {
-        labels = groups.map((_, i) => `Group ${i + 1}`);
-    }
-    
-    // Group means and overall mean
-    const groupMeans = groups.map(g => jStat.mean(g));
-    const allData = groups.flat();
-    const overallMean = jStat.mean(allData);
-    
-    // Sum of squares between groups (SSB)
-    const ssb = jStat.sum(groups.map((g, i) => g.length * Math.pow(groupMeans[i] - overallMean, 2)));
-    
-    // Sum of squares within groups (SSW)
-    const ssw = jStat.sum(groups.map((g, i) => 
-        jStat.sum(g.map(x => Math.pow(x - groupMeans[i], 2)))
-    ));
-    
-    // Total sum of squares
-    const sst = ssb + ssw;
-    
-    // Degrees of freedom
-    const dfBetween = k - 1;
-    const dfWithin = n - k;
-    const dfTotal = n - 1;
-    
-    // Mean squares
-    const msb = ssb / dfBetween;
-    const msw = ssw / dfWithin;
-    
-    // F-statistic and p-value
-    const fStat = msb / msw;
-    const pValue = 1 - jStat.centralF.cdf(fStat, dfBetween, dfWithin);
-    
-    // Effect size (eta-squared)
-    const etaSquared = ssb / sst;
-    
-    // Group statistics
-    const groupStats = groups.map((g, i) => ({
-        label: labels[i],
-        n: g.length,
-        mean: groupMeans[i],
-        variance: jStat.variance(g, true),
-        standardDeviation: jStat.stdev(g, true)
-    }));
-    
-    return {
-        testType: 'One-way ANOVA',
-        fStatistic: fStat,
-        pValue: pValue,
-        degreesOfFreedomBetween: dfBetween,
-        degreesOfFreedomWithin: dfWithin,
-        degreesOfFreedomTotal: dfTotal,
-        sumOfSquares: {
-            between: ssb,
-            within: ssw,
-            total: sst
-        },
-        meanSquares: {
-            between: msb,
-            within: msw
-        },
-        etaSquared: etaSquared,
-        groupStatistics: groupStats,
-        overallMean: overallMean,
-        totalSampleSize: n
-    };
+  if (!Array.isArray(groups) || groups.length < 2) {
+    return { error: 'Need at least 2 groups' };
+  }
+
+  const k = groups.length; // number of groups
+  const groupSizes = groups.map(g => g.length);
+  const n = jStat.sum(groupSizes); // total sample size
+
+  if (labels === null) {
+    labels = groups.map((_, i) => `Group ${i + 1}`);
+  }
+
+  // Group means and overall mean
+  const groupMeans = groups.map(g => jStat.mean(g));
+  const allData = groups.flat();
+  const overallMean = jStat.mean(allData);
+
+  // Sum of squares between groups (SSB)
+  const ssb = jStat.sum(groups.map((g, i) => g.length * Math.pow(groupMeans[i] - overallMean, 2)));
+
+  // Sum of squares within groups (SSW)
+  const ssw = jStat.sum(groups.map((g, i) =>
+    jStat.sum(g.map(x => Math.pow(x - groupMeans[i], 2)))
+  ));
+
+  // Total sum of squares
+  const sst = ssb + ssw;
+
+  // Degrees of freedom
+  const dfBetween = k - 1;
+  const dfWithin = n - k;
+  const dfTotal = n - 1;
+
+  // Mean squares
+  const msb = ssb / dfBetween;
+  const msw = ssw / dfWithin;
+
+  // F-statistic and p-value
+  const fStat = msb / msw;
+  const pValue = 1 - jStat.centralF.cdf(fStat, dfBetween, dfWithin);
+
+  // Effect size (eta-squared)
+  const etaSquared = ssb / sst;
+
+  // Group statistics
+  const groupStats = groups.map((g, i) => ({
+    label: labels[i],
+    n: g.length,
+    mean: groupMeans[i],
+    variance: jStat.variance(g, true),
+    standardDeviation: jStat.stdev(g, true)
+  }));
+
+  return {
+    testType: 'One-way ANOVA',
+    fStatistic: fStat,
+    pValue: pValue,
+    degreesOfFreedomBetween: dfBetween,
+    degreesOfFreedomWithin: dfWithin,
+    degreesOfFreedomTotal: dfTotal,
+    sumOfSquares: {
+      between: ssb,
+      within: ssw,
+      total: sst
+    },
+    meanSquares: {
+      between: msb,
+      within: msw
+    },
+    etaSquared: etaSquared,
+    groupStatistics: groupStats,
+    overallMean: overallMean,
+    totalSampleSize: n
+  };
 }
