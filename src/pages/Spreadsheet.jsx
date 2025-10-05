ea4faf11e5a8c08f1db3898619c3d6a4815932b1
Sebastian <sebastian.kuhlgert@gmail.com>
2025-10-06 00:33:34 +0200

    Change spreadsheet to RevoGrid

--- src/pages/Spreadsheet.jsx
+++ src/pages/Spreadsheet.jsx
@@ -1,222 +1,258 @@
-import { useEffect, useRef, useCallback } from 'react'
+import { useEffect, useMemo, useRef, useState, useCallback } from 'react'
 
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-
-import jspreadsheet from 'jspreadsheet-ce'
-import 'jspreadsheet-ce/dist/jspreadsheet.css'
+import { RevoGrid, Template } from '@revolist/react-datagrid'
 
 import { getFilteredData } from '../modules/database'
-
 import { useSelector } from 'react-redux'
+import useHelp from '../hooks/useHelp'
 
-import useHelp from "../hooks/useHelp";
+import { utils, writeFile } from 'xlsx'
+
+function normalizeCellValue(value) {
+  if (value === null || value === undefined) return ''; // show empty cell
+  if (Array.isArray(value)) return value.join(', ');
+  if (typeof value === 'object') return JSON.stringify(value);
+  return value; // primitives (string, number, etc.)
+}
 
 export default function Spreadsheet(props) {
-  const jssRef = useRef(null);
-
-  const stateDatasubsets = useSelector(state => state.datasubsets)
-  const stateParameters = useSelector(state => state.parameters)
-  const stateThresholds = useSelector(state => state.thresholds)
-  const stateAnalysis = useSelector(state => state.analysis)
+  const stateDatasubsets = useSelector((state) => state.datasubsets)
+  const stateParameters = useSelector((state) => state.parameters)
+  const stateThresholds = useSelector((state) => state.thresholds)
+  const stateAnalysis = useSelector((state) => state.analysis)
 
   const help = useHelp()
 
+  const [source, setSource] = useState([])
+  const [columns, setColumns] = useState([])
+
+  const gridRef = useRef(null)
+
+  /** =============================
+   *  BUILD COLUMNS & DATA
+   * ============================= */
   useEffect(() => {
+    const cols = stateParameters.map((col) => {
+      let columnType = 'text'
+      if (col.type === 'number') columnType = 'numeric'
+      if (col.specialtype === 'color') columnType = 'color'
+      if (col.specialtype === 'date-time') columnType = 'date'
+      if (!col.isSelected) columnType = 'hidden'
 
-    let columns = stateParameters.map((col) => {
-
-      let coltype = 'text'
-      if (col.type === 'number')
-        coltype = 'numeric'
-      if (col.specialtype === 'color')
-        coltype = 'color'
-      if (col.specialtype === 'date-time')
-        coltype = 'calendar'
-      if (!col.isSelected)
-        coltype = 'hidden'
-      return {
-        type: coltype,
-        name: col.name,
-        title: col.alias || col.name
+      let colDef = {
+        prop: col.name,
+        name: col.alias || col.name,
+        // size: 150,
+        columnType,
+        sortable: true,
+        // order: 'asc',
       }
-    });
-    
+
+      if (columnType === 'color')
+        colDef = {
+          ...colDef,
+          cellProperties: ({prop, model, data, column}) => {
+            return {
+              // Custom styles
+              style: {
+                color: model[prop]
+              },
+            }
+          }
+        }
+
+      // TODO: define more types in the future
+
+      return colDef;
+    })
+
     let data = []
 
-    if (stateDatasubsets.length === 0) {
-      data = getFilteredData('data', { thresholds: stateThresholds }).data({ removeMeta: true })
+    // Function 
+    const subsetID = (model) => {
+      try{
+        const modValue = stateDatasubsets.find( (itm) => itm.id === model.value);
+        return (<strong style={{color: modValue.color || 'red'}}>{modValue.name}</strong>)
+      }
+      catch(e){
+        console.log(e)
+      }
+      return (<>{model.value}</>)
     }
 
-    if (stateDatasubsets.length > 0) {
-      columns.unshift(
-        {
-          type: 'html',
-          name: 'subset',
-          title: 'Data Sub-Set'
+    if (stateDatasubsets.length === 0) {
+      data = getFilteredData('data', { thresholds: stateThresholds }).data({
+        removeMeta: true,
+      })
+    } else {
+      cols.unshift({
+        prop: 'subset',
+        name: 'Data Sub-Set',
+        size: 150,
+        columnType: 'text',
+        pin: 'colPinStart',
+        cellTemplate: Template(subsetID)
+      })
+
+      for (let series of stateDatasubsets) {
+        if (!series.isVisible) continue
+
+        const query = getFilteredData('data', {
+          filters: series.filter,
+          thresholds: stateThresholds,
         })
-      for (let series in stateDatasubsets) {
-        if (!stateDatasubsets[series].isVisible)
-          continue;
-        // TODO: Add Thresholds
-        let query = getFilteredData('data', { filters: stateDatasubsets[series].filter, thresholds: stateThresholds })
-        data = [...data, ...query.data({ removeMeta: true }).map((row) => { return { ...row, ...{ 'subset': `<span style="color:${stateDatasubsets[series].color}">${stateDatasubsets[series].name}</span>` } } })]
+
+        const subsetData = query
+          .data({ removeMeta: true })
+          .map((row) => ({
+            ...row,
+            subset: series.id,
+          }))
+
+        data = [...data, ...subsetData]
       }
     }
 
-    let options = {
-      data,
-      columns,
-      tableOverflow: true,
-      lazyLoading: true,
-      loadingSpin: true,
-      filters: false,
-      // freezeColumns: 1,
-      minDimensions: [10, 30],
-      defaultColWidth: '150px',
-      defaultColAlign: 'center',
-      tableWidth: '100%',
-      tableHeight: '100%',
-      allowExport: true,
-      editable: false,
-      selectionCopy: true,
-      search: true,
-      csvFileName: stateAnalysis?.saveAs,
-      contextMenu: function () {
-        return false;
-      },
-      onchange: changed,
-    };
+    // Normalize Data to make sure cell values are not causing
+    // an rendering issue with RevoGrid
+    const normalizedData = data.map((row) => {
+      const normalized = {};
+      for (const [key, val] of Object.entries(row)) {
+        normalized[key] = normalizeCellValue(val);
+      }
+      return normalized;
+    });
 
-    if (options.data.length > 0) {
-      let row = options.data.length;
-      let col = options.data[0].length;
-      options.minDimensions = [col, row];
-    }
+    setColumns(cols)
+    setSource(normalizedData)
 
-    console.time('Render Spreadsheet')
-    if (jssRef.current) {
-      if(jssRef.current.jspreadsheet)
-        jssRef.current.jspreadsheet.destroy();
-      jspreadsheet(jssRef.current, options)
-    }
-    console.timeEnd('Render Spreadsheet')
+  }, [stateParameters, stateDatasubsets, stateThresholds])
 
-    if(document.querySelector('.jexcel_search')){
-      document.querySelector('.jexcel_search').type = 'search'
-      document.querySelector('.jexcel_search').classList.add("form-control", "form-control-sm");
-    }
+  /** =============================
+   *  EXPORT HANDLERS
+   * ============================= */
 
-    // TODO: use .showColumn or .hideColumn if only the visibility changes
-
-    // console.log(jssRef.current.jexcel.getConfig())
-
-  }, [stateParameters, stateDatasubsets, stateThresholds]);
-
-  const handleClickHelp = useCallback( ()=>{
-    help.open("Help | Spreadsheet", "help/md/spreadsheet.md")
-  },[] )
-
-  function changed(instance, cell, x, y, value) {
-    // console.log(instance, cell, x, y, value)
-    // https://bossanova.uk/jspreadsheet/v4/docs/programmatically-changes
-    // let headers = jssRef.current.jexcel.getHeaders();
-
-    // console.log(jssRef.current.jexcel.getConfig())
-
-  }
-
-  // Download Data as CSV
   function downloadCSV() {
-    jssRef.current.jexcel.download(true);
+    const ws = utils.json_to_sheet(source)
+    const csv = utils.sheet_to_csv(ws)
+    triggerDownload(csv, `${stateAnalysis?.saveAs || 'data'}.csv`, 'text/csv')
   }
 
-  // Download Data as Tab
   function downloadTXT() {
-    // Get data
-    let data = ''
-    data += jssRef.current.jexcel.copy(false, '\t', true, true, true);
-
-    let filename = jssRef.current.jexcel.options.csvFileName + '.txt'
-
-    // Download element
-    var blob = new Blob(["\uFEFF" + data], { type: 'text/txt;charset=utf-8;' });
-
-    // IE Compatibility
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, filename);
-    } else {
-      // Download element
-      var pom = document.createElement('a');
-      var url = URL.createObjectURL(blob);
-      pom.href = url;
-      pom.setAttribute('download', filename);
-      document.body.appendChild(pom);
-      pom.click();
-      pom.parentNode.removeChild(pom);
-    }
+    const ws = utils.json_to_sheet(source)
+    const txt = utils.sheet_to_txt ? utils.sheet_to_txt(ws) : utils.sheet_to_csv(ws, { FS: '\t' })
+    triggerDownload(txt, `${stateAnalysis?.saveAs || 'data'}.txt`, 'text/plain')
   }
 
   function downloadJSON() {
-    // Get data
-    let headers = jssRef.current.jexcel.getHeaders();
-    let json = jssRef.current.jexcel.getJson();
-
-    json.map((el) => {
-      Object.keys(el).forEach(function (key) {
-        return headers.includes(key) || delete el[key];
-      });
-      return el;
-    });
-
-    let data = ''
-    data += JSON.stringify(json, null, 2);
-    let filename = jssRef.current.jexcel.options.csvFileName + '.json'
-
-    // Download element
-    var blob = new Blob(["\uFEFF" + data], { type: 'text/JSON;charset=utf-8;' });
-
-    // IE Compatibility
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-      window.navigator.msSaveOrOpenBlob(blob, filename);
-    } else {
-      // Download element
-      var pom = document.createElement('a');
-      var url = URL.createObjectURL(blob);
-      pom.href = url;
-      pom.setAttribute('download', filename);
-      document.body.appendChild(pom);
-      pom.click();
-      pom.parentNode.removeChild(pom);
-    }
+    const jsonStr = JSON.stringify(source, null, 2)
+    triggerDownload(jsonStr, `${stateAnalysis?.saveAs || 'data'}.json`, 'application/json')
   }
+
+  function downloadXLSX() {
+    const ws = utils.json_to_sheet(source);
+    const wb = utils.book_new();
+    utils.book_append_sheet(wb, ws, 'Sheet1');
+    const filename = `${stateAnalysis?.saveAs || 'data'}.xlsx`;
+    writeFile(wb, filename);
+  }
+
+  function triggerDownload(dataStr, filename, mime) {
+    const blob = new Blob(['\uFEFF' + dataStr], { type: mime })
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = url
+    a.setAttribute('download', filename)
+    document.body.appendChild(a)
+    a.click()
+    a.remove()
+  }
+
+  /** =============================
+   *  HELP BUTTON HANDLER
+   * ============================= */
+  const handleClickHelp = useCallback(() => {
+    help.open('Help | Spreadsheet', 'help/md/spreadsheet.md')
+  }, [])
+
+  /** =============================
+   *  RENDER
+   * ============================= */
 
   return (
     <>
       <Row>
-        <Col className='pt-2'>
-          <ButtonToolbar aria-label="Spreadsheet Menu" className='d-flex align-items-center'>
+        <Col className='py-2'>
+          <ButtonToolbar
+            aria-label='Spreadsheet Menu'
+            className='d-flex align-items-center'
+          >
             <label className='me-2 fw-bold'>Save as</label>
-            <ButtonGroup size='sm' className="me-2" aria-label="Download Data">
-              <Button variant={props.darkmode? "outline-light" : "outline-dark"} onClick={downloadCSV}><i className='bi-filetype-csv' /> CSV</Button>
-              <Button variant={props.darkmode? "outline-light" : "outline-dark"} onClick={downloadTXT}><i className='bi-filetype-txt' /> Text</Button>
+            <ButtonGroup
+              size='sm'
+              className='me-2'
+              aria-label='Download Data'
+            >
+              <Button
+                variant={props.darkmode ? 'outline-light' : 'outline-dark'}
+                onClick={downloadCSV}
+              >
+                <i className='bi-filetype-csv' /> CSV
+              </Button>
+              <Button
+                variant={props.darkmode ? 'outline-light' : 'outline-dark'}
+                onClick={downloadTXT}
+              >
+                <i className='bi-filetype-txt' /> Text
+              </Button>
+              <Button
+                variant={props.darkmode ? 'outline-light' : 'outline-dark'}
+                onClick={downloadXLSX}
+              >
+                <i className='bi-filetype-xlsx' /> Excel
+              </Button>
             </ButtonGroup>
-            <ButtonGroup size='sm' aria-label="Third group">
-              <Button variant={props.darkmode? "outline-light" : "outline-dark"} onClick={downloadJSON}><i className='bi-filetype-json' /> JSON</Button>
+            <ButtonGroup size='sm' aria-label='Third group'>
+              <Button
+                variant={props.darkmode ? 'outline-light' : 'outline-dark'}
+                onClick={downloadJSON}
+              >
+                <i className='bi-filetype-json' /> JSON
+              </Button>
             </ButtonGroup>
-            <ButtonGroup className='ms-2' size='sm' aria-label="Help Group">
-              <Button variant={props.darkmode? "outline-light" : "outline-dark"} onClick={handleClickHelp}><i className='bi-question-circle' /></Button>
+            <ButtonGroup className='ms-2' size='sm' aria-label='Help Group'>
+              <Button
+                variant={props.darkmode ? 'outline-light' : 'outline-dark'}
+                onClick={handleClickHelp}
+              >
+                <i className='bi-question-circle' />
+              </Button>
             </ButtonGroup>
           </ButtonToolbar>
         </Col>
       </Row>
       <Row>
         <Col sm='12' className='spreadsheet-container'>
-          <div ref={jssRef} />
+          <RevoGrid
+            ref={gridRef}
+            className='color-grid cell-border'
+            theme={props.darkmode ? 'darkCompact' : 'compact'}
+            columns={columns}
+            source={source}
+            resize
+            range
+            filter={false}
+            readonly
+            autoSizeColumn
+            sorting={true}
+            exporting={true}
+          />
         </Col>
       </Row>
     </>
