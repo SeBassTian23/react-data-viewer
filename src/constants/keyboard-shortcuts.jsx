45e624d6502f560ccc052a7a8ea211378105e0aa
Sebastian <sebastian.kuhlgert@gmail.com>
2025-10-11 11:39:53 +0200

    update old shortcuts

--- src/constants/keyboard-shortcuts.jsx
+++ src/constants/keyboard-shortcuts.jsx
@@ -1,37 +1,37 @@
 export const KEYBOARD_SHORTCUTS = {
   newAnalysis: {
     apple: "^ ⌘ N",
-    windows: "Alt + Ctrl + N",
+    windows: "Alt+Ctrl+N",
     description: "New Analysis"
   },
   saveAnalysis: {
     apple: "⌘ S",
-    windows: "Ctrl + S",
+    windows: "Ctrl+S",
     description: "Save Analysis"
   },
   saveBookmark: {
     apple: "⇧ ⌘ S",
-    windows: "Ctrl + Shift + S",
+    windows: "Ctrl+Shift+S",
     description: "Save Bookmark"
   },
   showBookmarks: {
     apple: "⇧ ⌘ L",
-    windows: "Ctrl + Shift + L",
+    windows: "Ctrl+Shift+L",
     description: "Bookmarks"
   },
   loadAnalysis: {
     apple: "⌘ L",
-    windows: "Ctrl + L",
+    windows: "Ctrl+L",
     description: "Load Analysis"
   },
   importData: {
     apple: "⌘ I",
-    windows: "Ctrl + I",
+    windows: "Ctrl+I",
     description: "Import Data"
   },
   toggleFilter: {
     apple: "⌘ K",
-    windows: "Ctrl + K",
+    windows: "Ctrl+K",
     description: "Toggle Filter/Data Tab"
   },
   showDashboard: {
