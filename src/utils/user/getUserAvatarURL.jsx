60b5f8640dddfb464ec29901606cedf91bce4429
Sebastian <sebastian.kuhlgert@gmail.com>
2026-01-17 11:40:05 +0100

    update url generation with email check and set size

--- src/utils/user/getUserAvatarURL.jsx
+++ src/utils/user/getUserAvatarURL.jsx
@@ -1,9 +1,9 @@
 import md5 from '../../helpers/md5';
 
-export const getUserAvatarURL = (email) => {
-  if (!email) return '';
+export const getUserAvatarURL = (email, size=null) => {
+  if (!email || !email.match(/^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm)) return null;
 
   const emailHash = md5(email);
 
-  return `https://www.gravatar.com/avatar/${emailHash}?d=initials`;
+  return `https://www.gravatar.com/avatar/${emailHash}?d=initials${size? '&s='+String(size) : ''}`;
 };
