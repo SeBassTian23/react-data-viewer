e9bc432ab30a82ed2495fbf84a2888a6861c4e42
Sebastian <sebastian.kuhlgert@gmail.com>
2026-01-27 14:10:22 +0100

    improving footer layout

--- src/components/Sidebar/SidebarFooter.jsx
+++ src/components/Sidebar/SidebarFooter.jsx
@@ -39,13 +39,13 @@ export default function SidebarFooter(props) {
       <Col sm={12} className='border-top d-flex justify-content-evenly align-items-center'>
         <span className='small p-2 px-0' title='Total Number of Rows (Data-Sets)'><i className={count > 0 ? 'bi-database-fill-check' : 'bi-database-fill'} /> {count}</span>
         <span className='small p-2 px-0' title='Parameters - Active (Deactivated)'><i className='bi-toggles' /> {stateParameters.filter(x => x.isSelected).length}
-          {(stateParameters.length !== stateParameters.filter(x => x.isSelected).length) && <>({stateParameters.length - stateParameters.filter(x => x.isSelected).length})</>}
+          {(stateParameters.length !== stateParameters.filter(x => x.isSelected).length) && <span style={{fontSize: 'x-small'}}>({stateParameters.length - stateParameters.filter(x => x.isSelected).length})</span>}
         </span>
         <span className={stateThresholds.filter(x => x.isSelected).length > 0 ? 'small p-2 px-0 text-danger' : 'small px-0 p-2'} title='Thresholds - Active|Total'><i className='bi-bar-chart-steps' /> {stateThresholds.filter(x => x.isSelected).length}
-          {(stateThresholds.length !== stateThresholds.filter(x => x.isSelected).length) && <>({stateThresholds.length - stateThresholds.filter(x => x.isSelected).length})</>}
+          {(stateThresholds.length !== stateThresholds.filter(x => x.isSelected).length) && <span style={{fontSize: 'x-small'}}>({stateThresholds.length - stateThresholds.filter(x => x.isSelected).length})</span>}
         </span>
         <span className='small p-2 px-0' title='Bookmarks'><i className='bi bi-journal-bookmark-fill' /> {stateBookmarks.length}</span>
-        <span className='small p-2 px-0 d-flex align-items-center' title='Resources (Memory)'><i className='bi bi-cpu' /> <span style={{fontSize: '0.5rem', lineHeight: '0.5rem', width: '1rem', padding: '0 0 0 0.2rem'}} className="d-flex flex-column w-100 text-end">
+        <span className='small p-2 px-0 d-flex align-items-center' title='Resources (Memory)'><i className='bi bi-cpu' /> <span className="d-flex flex-column w-100 text-end footer-memory">
             <span className='text-nowrap'>{memory[0]}</span>
             <span className='text-nowrap'>{memory[1]}</span>
           </span>
