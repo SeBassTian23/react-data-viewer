bdb146cd1da0e642ef9acf902cd632e48bf7fa38
Sebastian <sebastian.kuhlgert@gmail.com>
2026-02-10 11:06:36 +0100

    fix keydown event for modal confirmation dialog

--- src/components/Dialogs/ModalDialogConfirm.jsx
+++ src/components/Dialogs/ModalDialogConfirm.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {useEffect} from 'react'
 
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal';
 
 export default function ModalDialogConfirm(props) {
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'Enter') {
+        props.onHide(true);
+      }
+    };
+
+    if (props.show) {
+      document.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [props.show, props]);
+
   return (
     <Modal
       {...props}
@@ -15,7 +32,7 @@ export default function ModalDialogConfirm(props) {
         {props.content && <p className="mb-0 small">{props.content}</p>}
       </Modal.Body>
       <Modal.Footer className='flex-nowrap p-0'>
-        <Button onClick={() => props.onHide(true)} onKeyDown={(e) => { if (e.key === 'enter') props.onHide(true) }} variant='link' className='fs-6 text-decoration-none col-6 m-0 rounded-0 border-end'><strong className="text-danger">{props.yes || 'Yes'}</strong></Button>
+        <Button onClick={() => props.onHide(true)} variant='link' className='fs-6 text-decoration-none col-6 m-0 rounded-0 border-end'><strong className="text-danger">{props.yes || 'Yes'}</strong></Button>
         <Button onClick={() => props.onHide(false)} variant='link' className='fs-6 text-decoration-none col-6 m-0 rounded-0 border-end'>{props.no || 'No'}</Button>
       </Modal.Footer>
     </Modal>
