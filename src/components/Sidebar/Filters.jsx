9dd79172afd650c24e9d98dc049728b83265afcc
Sebastian <sebastian.kuhlgert@gmail.com>
2026-02-10 17:49:56 +0100

    Separate out functions for easier handling

--- src/components/Sidebar/Filters.jsx
+++ src/components/Sidebar/Filters.jsx
@@ -32,7 +32,6 @@ export default function Filters() {
   const toast = useToast()
   const help = useHelp();
 
-
   const resetSelection = (event) => {
     event.preventDefault()
     // Add Filter to Series
@@ -43,108 +42,114 @@ export default function Filters() {
     reset()
   }
 
-  const handleClickHelp = useCallback( ()=>{
+  const handleClickHelp = useCallback(() => {
     help.open("Help | Filter Data", "help/md/data-filter.md")
-  },[] )
+  }, [])
 
-  return (
-    <>
-      <Row id='dv-filters'>
-        <Col sm={12} className="my-2 border-bottom d-flex justify-content-between align-items-center fw-bold">
-          Filter Data into Subsets
-          <Button variant={null} onClick={handleClickHelp}><i className='bi-question-circle' /></Button>
-        </Col>
-        <Col sm={12}>
-          <ButtonToolbar aria-label="Filters Menu">
-            <ButtonGroup size='sm' className="me-2" aria-label="Apply Filter">
-              <Button variant='outline-secondary' onClick={handleSubmit((data) => {
-                
-                if (Object.entries(data).find((item) => item[1].length > 0)) {          
-                  const selection = {
-                    name: Object.entries(data).filter((item) => {
-                      return item[1].length > 0
-                    }).map((item) => item[1] ).join(' & '),
-                    count: 0,
-                    isVisible: true,
-                    filter: Object.entries(data).filter((item) => item[1].length > 0).map((item) => {
-                      const dataType = state.find( param => param.name === item[0] || param.alias === item[0] )?.specialtype || state.find( param => param.name === item[0] || param.alias === item[0] )?.type || undefined
-                      return {
-                        'name': item[0],
-                        'values': ['latitude','longitude', 'number'].indexOf(dataType) > -1? item[1].map(itm => Number(itm)): item[1],
-                        'type': dataType
-                      }
-                    })
-                  }
 
-                  selection.count = getFilteredData('data', { filters: selection.filter }).data().length
+  const handleClickSingle = handleSubmit((data) => {
 
-                  // Add Filter to Series
-                  dispatch(datasubsetAdded(selection))
+    if (Object.entries(data).find((item) => item[1].length > 0)) {
+      const selection = {
+        name: Object.entries(data).filter((item) => {
+          return item[1].length > 0
+        }).map((item) => item[1]).join(' & '),
+        count: 0,
+        isVisible: true,
+        filter: Object.entries(data).filter((item) => item[1].length > 0).map((item) => {
+          const dataType = state.find(param => param.name === item[0] || param.alias === item[0])?.specialtype || state.find(param => param.name === item[0] || param.alias === item[0])?.type || undefined
+          const dataTypeOrg = state.find(param => param.name === item[0] || param.alias === item[0])?.type
+          return {
+            'name': item[0],
+            'values': [dataType, dataTypeOrg].some(type => ['latitude', 'longitude', 'number'].includes(type)) ? item[1].map(itm => Number(itm)) : item[1],
+            'type': dataType
+          }
+        })
+      }
 
-                  toast.success( "A subset has been added" , "Filter", "bi-filter");
-                  
-                  const fields = Object.keys(getValues()) || []
-                  fields.forEach(element => {
-                    resetField(element)
-                  });
-                  reset()
-                }
-              })}><i className='bi-union' /> Single</Button>
+      selection.count = getFilteredData('data', { filters: selection.filter }).data().length
 
-              <Button variant='outline-secondary' onClick={handleSubmit((data, event) => {
+      // Add Filter to Series
+      dispatch(datasubsetAdded(selection))
 
-                if (Object.entries(data).find((item) => item[1].length > 0)) {
-                  const selections = Object.entries(data).filter((item) => item[1].length > 0)
-                  const filters = selections.map((item) => {
-                    const dataType = state.find( param => param.name === item[0] || param.alias === item[0] )?.specialtype || state.find( param => param.name === item[0] || param.alias === item[0] )?.type || undefined
-                    return {
-                      'name': item[0],
-                      'values': ['latitude','longitude', 'number'].indexOf(dataType) > -1? item[1].map(itm => Number(itm)): item[1],
-                      'type': dataType
-                    }
-                  })
+      toast.success("A subset has been added", "Filter", "bi-filter");
 
-                  if (selections.length > 0) {
+      const fields = Object.keys(getValues()) || []
+      fields.forEach(element => {
+        resetField(element)
+      });
+      reset()
+    }
+  })
 
-                    const selection = generateFilterCombinations(filters).map((item) => {
+  const handleClickSeparate = handleSubmit((data) => {
 
-                      return {
-                        name: item.map((item) => item.values.join(', ')).join(' & '),
-                        count: getFilteredData('data', { filters: item }).data().length,
-                        isVisible: true,
-                        filter: item
-                      }
+    if (Object.entries(data).find((item) => item[1].length > 0)) {
+      const selections = Object.entries(data).filter((item) => item[1].length > 0)
+      const filters = selections.map((item) => {
+        const dataType = state.find(param => param.name === item[0] || param.alias === item[0])?.specialtype || state.find(param => param.name === item[0] || param.alias === item[0])?.type || undefined
+        const dataTypeOrg = state.find(param => param.name === item[0] || param.alias === item[0])?.type
+        return {
+          'name': item[0],
+          'values': [dataType, dataTypeOrg].some(type => ['latitude', 'longitude', 'number'].includes(type)) ? item[1].map(itm => Number(itm)) : item[1],
+          'type': dataType
+        }
+      })
 
-                    })
+      if (selections.length > 0) {
 
-                    // Add Filter to Series
-                    dispatch(datasubsetMultipleAdded(selection))
+        const selection = generateFilterCombinations(filters).map((item) => {
 
-                    toast.success( selection.length === 1? "A subset has been added" : `${selection.length} subsets have been added` , "Filter", "bi-filter");
+          return {
+            name: item.map((item) => item.values.join(', ')).join(' & '),
+            count: getFilteredData('data', { filters: item }).data().length,
+            isVisible: true,
+            filter: item
+          }
 
-                    const fields = Object.keys(getValues()) || []
-                    fields.forEach(element => {
-                      resetField(element)
-                    });
-                    reset();
-                  }
-                }
-              })}><i className='bi-subtract' /> Separate</Button>
-            </ButtonGroup>
-            <ButtonGroup size='sm' className="me-2" aria-label="Selection">
-              <Button variant='outline-secondary' onClick={resetSelection}><i className="bi-x-circle" /> Reset</Button>
-            </ButtonGroup>
-          </ButtonToolbar>
-        </Col>
-      </Row>
+        })
 
-      <Row className={`h-100 overflow-auto${(state.length === 0) ? ' align-items-center' : ''}`}>
-        <Col sm={12} className='p-0'>
-          {state.length === 0? <div className='text-center'>
-              <i className='bi-filter text-muted fs-1' />
-              <span className='d-block text-muted fs-5'>Filters</span>
-              <p className='small'>No Filters available</p>
-            </div>
+        // Add Filter to Series
+        dispatch(datasubsetMultipleAdded(selection))
+
+        toast.success(selection.length === 1 ? "A subset has been added" : `${selection.length} subsets have been added`, "Filter", "bi-filter");
+
+        const fields = Object.keys(getValues()) || []
+        fields.forEach(element => {
+          resetField(element)
+        });
+        reset();
+      }
+    }
+  })
+
+return (
+  <>
+    <Row id='dv-filters'>
+      <Col sm={12} className="my-2 border-bottom d-flex justify-content-between align-items-center fw-bold">
+        Filter Data into Subsets
+        <Button variant={null} onClick={handleClickHelp}><i className='bi-question-circle' /></Button>
+      </Col>
+      <Col sm={12}>
+        <ButtonToolbar aria-label="Filters Menu">
+          <ButtonGroup size='sm' className="me-2" aria-label="Apply Filter">
+            <Button variant='outline-secondary' onClick={handleClickSingle}><i className='bi-union' /> Single</Button>
+            <Button variant='outline-secondary' onClick={handleClickSeparate} ><i className='bi-subtract' /> Separate</Button>
+          </ButtonGroup>
+          <ButtonGroup size='sm' className="me-2" aria-label="Selection">
+            <Button variant='outline-secondary' onClick={resetSelection}><i className="bi-x-circle" /> Reset</Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+      </Col>
+    </Row>
+
+    <Row className={`h-100 overflow-auto${(state.length === 0) ? ' align-items-center' : ''}`}>
+      <Col sm={12} className='p-0'>
+        {state.length === 0 ? <div className='text-center'>
+          <i className='bi-filter text-muted fs-1' />
+          <span className='d-block text-muted fs-5'>Filters</span>
+          <p className='small'>No Filters available</p>
+        </div>
           :
           <ListGroup as="ul" variant="flush" className={`h-100`}>
             {state.map((el, idx) => {
@@ -156,8 +161,8 @@ export default function Filters() {
             })}
           </ListGroup>
         }
-        </Col>
-      </Row>
-    </>
-  )
+      </Col>
+    </Row>
+  </>
+)
 }
