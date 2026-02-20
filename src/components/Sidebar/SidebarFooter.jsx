7b9419778699a1c90e8ecdfda4786b8a1a2aff55
Sebastian <sebastian.kuhlgert@gmail.com>
2026-02-20 22:53:47 +0100

    simplify code

--- src/components/Sidebar/SidebarFooter.jsx
+++ src/components/Sidebar/SidebarFooter.jsx
@@ -11,19 +11,22 @@ import { getDatasetCount } from '../../modules/database'
 
 export default function SidebarFooter(props) {
 
-  const [count, setCount] = useState(0)
-
   const stateParameters = useSelector(state => state.parameters)
   const stateThresholds = useSelector(state => state.thresholds)
   const stateDatasubsets = useSelector(state => state.datasubsets)
   const stateBookmarks = useSelector(state => state.bookmarks)
 
+  const [count, setCount] = useState(0);
+  const [parameters, setParameters] = useState([0,0]);
+  const [memory, setMemory] = useState([0,0]);
+
   useEffect(() => {
     setCount(getDatasetCount())
+    setParameters([
+      stateParameters.length,
+      stateParameters.filter(x => x.isSelected).length
+    ])
   }, [stateParameters, stateDatasubsets, stateThresholds, stateBookmarks])
-
-
-  const [memory, setMemory] = useState([0,0]);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -36,16 +39,12 @@ export default function SidebarFooter(props) {
 
   return (
     <Row id="dv-sidebar-footer">
-      <Col sm={12} className='border-top d-flex justify-content-evenly align-items-center'>
-        <span className='small p-2 px-0' title='Total Number of Rows (Data-Sets)'><i className={count > 0 ? 'bi-database-fill-check' : 'bi-database-fill'} /> {count}</span>
-        <span className='small p-2 px-0' title='Parameters - Active (Deactivated)'><i className='bi-toggles' /> {stateParameters.filter(x => x.isSelected).length}
-          {(stateParameters.length !== stateParameters.filter(x => x.isSelected).length) && <span style={{fontSize: 'x-small'}}>({stateParameters.length - stateParameters.filter(x => x.isSelected).length})</span>}
-        </span>
-        <span className={stateThresholds.filter(x => x.isSelected).length > 0 ? 'small p-2 px-0 text-danger' : 'small px-0 p-2'} title='Thresholds - Active|Total'><i className='bi-bar-chart-steps' /> {stateThresholds.filter(x => x.isSelected).length}
-          {(stateThresholds.length !== stateThresholds.filter(x => x.isSelected).length) && <span style={{fontSize: 'x-small'}}>({stateThresholds.length - stateThresholds.filter(x => x.isSelected).length})</span>}
-        </span>
-        <span className='small p-2 px-0' title='Bookmarks'><i className='bi bi-journal-bookmark-fill' /> {stateBookmarks.length}</span>
-        <span className='small p-2 px-0 d-flex align-items-center' title='Resources (Memory)'><i className='bi bi-cpu' /> <span className="d-flex flex-column w-100 text-end footer-memory">
+      <Col sm={12} className='border-top d-flex justify-content-evenly align-items-center small py-2'>
+        <span title='Total Number of Rows (Data-Sets)'><i className={count > 0 ? 'bi-database-fill-check' : 'bi-database-fill'} /> {count}</span>
+        <span title={`Parameters (${parameters.join("|")})`}><i className='bi bi-toggles' /> {parameters[1]}</span>
+        <span className={`${stateThresholds.filter(x => x.isSelected).length > 0 ? 'text-danger' : ''}`} title='Thresholds'><i className='bi-bar-chart-steps' /> {stateThresholds.filter(x => x.isSelected).length}</span>
+        <span title='Bookmarks'><i className='bi bi-journal-bookmark-fill' /> {stateBookmarks.length}</span>
+        <span className='d-flex align-items-center' title='Resources (Memory)'><i className='bi bi-cpu' /> <span className="d-flex flex-column w-100 text-end footer-memory">
             <span className='text-nowrap'>{memory[0]}</span>
             <span className='text-nowrap'>{memory[1]}</span>
           </span>
