7cd2b18566c80166bfe97bcbd0a925f16fe4c52c
Sebastian <sebastian.kuhlgert@gmail.com>
2026-02-27 01:01:28 +0100

    Improve documentation code and add chapters

---
+++ src/constants/documentation.jsx
@@ -0,0 +1,21 @@
+/**
+ * Chapter order for documentation.
+ */
+export default [
+  { url: "help/md/getting-started.md" },
+  { url: "help/md/analysis.md" },
+  { url: "help/md/data-import.md" },
+  { url: "help/md/dashboard.md" },
+  { url: "help/md/plot.md" },
+  { url: "help/md/map.md" },
+  { url: "help/md/data-filter.md" },
+  { url: "help/md/data-subsets.md" },
+  { url: "help/md/data-flag.md" },
+  { url: "help/md/bookmarks.md" },
+  { url: "help/md/aliases.md" },
+  { url: "help/md/data-types.md" },
+  { url: "help/md/parameters.md" },
+  { url: "help/md/spreadsheet.md" },
+  { url: "help/md/thresholds.md" },
+  { url: "help/md/shortcuts.md" }
+]
