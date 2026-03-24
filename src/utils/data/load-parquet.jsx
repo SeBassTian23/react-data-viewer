c3117fe62c51c3508e0ef4d833cbf386bf91171f
Sebastian <sebastian.kuhlgert@gmail.com>
2026-03-24 11:03:47 +0100

    fix improper handling of timestamps

--- src/utils/data/load-parquet.jsx
+++ src/utils/data/load-parquet.jsx
@@ -1,6 +1,12 @@
 import { parquetMetadata, parquetRead } from 'hyparquet';
 
-const normalize = (value) => {
+const normalize = (value, type) => {
+
+  // Convert legacy timestamps properly
+  if(type == 'INT96'){
+    return new Date(value).toISOString()
+  }
+
   // convert BigInt
   if (typeof value === "bigint") {
     return Number(value);
@@ -21,10 +27,14 @@ const normalize = (value) => {
   return value;
 };
 
-const normalizeRow = (row) =>
-  Object.fromEntries(
-    Object.entries(row).map(([k, v]) => [k, normalize(v)])
+const normalizeRow = (row, schema) => {
+  return Object.fromEntries(
+    Object.entries(row).map(([k, v]) => {
+      const metaType = schema.find( itm => itm.name == k)?.type
+      return [k, normalize(v,metaType)]
+    })
   );
+}
 
 const loadPARQUET = async (file) => {
   
@@ -38,7 +48,7 @@ const loadPARQUET = async (file) => {
     metadata,
     rowFormat: 'object',
     onComplete: (data) => {
-      rows = data.map( row => normalizeRow(row) );
+      rows = data.map( row => normalizeRow(row, metadata.schema) );
     },
   });
 
